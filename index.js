4d788a9889fa9fc7cfdccf4f6b752a29aef3defa
jordan-massingill <jordan_massingill@hotmail.com>
2018-08-28 13:01:25 -0500

    Some changes to the mini project

--- index.js
+++ index.js
@@ -20,6 +20,22 @@ server.get('/users', (req, res) => {
 
     res.status(500).json({ message: 'Error retrieving data'})
   });
+});
+
+server.post('/users', async (req, res) => {
+  const user = req.body;
+  if (user.name && user.bio) {
+    try {
+      const response = await db.insert(user);
+      res.status(201).json({ message: "User was successfully added"});
+    }
+    catch (err) {
+      res.status(500).json({ message: "Error adding user"});
+    }
+  }
+  else {
+    res.status(422).json({ message: "Must have both name and bio to add user"});
+  }
 })
 
 server.listen(9000, ()=>console.log('Hello!'))
